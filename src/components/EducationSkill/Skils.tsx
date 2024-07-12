8f8afec9dd7df5a3c91021585be66c0e4243b2c1
Soner <syln.soner@gmail.com>
2024-07-12 16:12:14 +0300

    chore: Update education and skills section with correct information

--- src/components/EducationSkill/Skils.tsx
+++ src/components/EducationSkill/Skils.tsx
@@ -8,59 +8,61 @@ const Skils = () => {
   return (
     <div className="pt-[4rem] md:pt-[8rem] pb-[5rem] bg-[#09101a]">
       <h1 className="heading">
-        DENEYİM & <span className="text-yellow-400">BECERİLER</span>
+        EĞİTİM & <span className="text-yellow-400">BECERİLER</span>
       </h1>
-      <div className="w-[80%] mx-auto pt-[4rem] md:pt-[8rem] grid grid-cols-1 md:grid-cols-2 gap-[2rem] items-center">
+      <div className="w-[80%] mx-auto pt-[4rem] md:pt-[8rem] grid grid-cols-1 md:grid-cols-3 gap-[2rem] items-center">
         <div>
           <SkilsItem
             title="Codecademy"
             year="2023 - 2024"
             description="Java & React Full Stack Trainee"
           />
+
+          <SkilsLanguage
+            skill1="Java"
+            skill2="Spring Boot"
+            skill3="Git & GitHub"
+            skill4="React"
+            level1="w-[81%]"
+            level2="w-[77%]"
+            level3="w-[90%]"
+            level4="w-[60%]"
+          />
+        </div>
+
+        <div>
           <SkilsItem
             title="İstanbul Kodluyor (Tobeto)"
             year="2023 - 2024"
             description="Java & React Full Stack Trainee"
           />
           <SkilsLanguage
-            skill1="Java"
-            skill2="Spring Boot"
-            skill3="Git & GitHub"
-            skill4="Hibernate"
-            skill5="Linux"
-            skill6="Agile"
-            level1="w-[81%]"
-            level2="w-[77%]"
-            level3="w-[90%]"
-            level4="w-[79%]"
-            level5="w-[70%]"
-            level6="w-[55%]"
+            skill1="Hibernate"
+            skill2="CSS"
+            skill3="Agile"
+            skill4="TypeScript"
+            level1="w-[79%]"
+            level2="w-[55%]"
+            level3="w-[55%]"
+            level4="w-[55%]"
           />
         </div>
+
         <div>
-          <SkilsItem
-            title="BAYKAR TEKNOLOJİ"
-            year="2022"
-            description="R&D Engineer"
-          />
           <SkilsItem
             title="İSTANBUL GELİŞİM ÜNİVERSİTESİ"
             year="2016 - 2021"
             description="Mühendislik Fakültesi/Elektrik Elektronik Mühendisi"
           />
           <SkilsLanguage
-            skill1="React"
-            skill2="TypeScript"
-            skill3="CSS"
-            skill4="CentOS"
-            skill5="matlab"
-            skill6="SCRUM"
-            level1="w-[60%]"
-            level2="w-[55%]"
-            level3="w-[55%]"
-            level4="w-[70%]"
-            level5="w-[80%]"
-            level6="w-[55%]"
+            skill1="CentOS"
+            skill2="Matlab"
+            skill3="Linux"
+            skill4="SCRUM"
+            level1="w-[70%]"
+            level2="w-[80%]"
+            level3="w-[70%]"
+            level4="w-[55%]"
           />
         </div>
       </div>
--- src/components/EducationSkill/SkilsLanguage.tsx
+++ src/components/EducationSkill/SkilsLanguage.tsx
@@ -5,14 +5,10 @@ interface Props {
   skill2: string;
   skill3: string;
   skill4: string;
-  skill5: string;
-  skill6: string;
   level1: string;
   level2: string;
   level3: string;
   level4: string;
-  level5: string;
-  level6: string;
 }
 
 const SkilsLanguage = ({
@@ -20,14 +16,10 @@ const SkilsLanguage = ({
   skill2,
   skill3,
   skill4,
-  skill5,
-  skill6,
   level1,
   level2,
   level3,
   level4,
-  level5,
-  level6,
 }: Props) => {
   return <div>
     <div className="relative mb-[3rem] ">
@@ -56,19 +48,7 @@ const SkilsLanguage = ({
         <span className={`${level4} bottom-0 h-[6px] absolute bg-[#55e6a5] `}></span>
     </div>
 
-    <div className="relative mb-[3rem] ">
-        <h1 className="p-5 w-[100%] uppercase bg-gray-800 rounded-sm text-white text-[20px] font-bold">
-            {skill5}
-        </h1>
-        <span className={`${level5} bottom-0 h-[6px] absolute bg-[#55e6a5] `}></span>
-    </div>
-
-    <div className="relative mb-[3rem] ">
-        <h1 className="p-5 w-[100%] uppercase bg-gray-800 rounded-sm text-white text-[20px] font-bold">
-            {skill6}
-        </h1>
-        <span className={`${level6} bottom-0 h-[6px] absolute bg-[#55e6a5] `}></span>
-    </div>
+    
     
   </div>;
 };
